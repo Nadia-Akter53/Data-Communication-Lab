3b7bc3e2c44187810cd2174e09b014e9cdf1f943
Nadia Akter Eshita <eshitasundor@gmail.com>
2025-05-17 19:12:39 +0600

    Create Polynomial_CRC_in_C.c

---
+++ Polynomial_CRC_in_C.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+
+char data[20], div[20], temp[100], total[100];
+int i, j, datalen, divlen, len, flag = 1;
+
+void check();
+void printPolynomial(char divisor[], int length);
+
+int main() {
+    printf("Enter the total bit of data: ");
+    scanf("%d", &datalen);
+    printf("\nEnter the total bit of divisor: ");
+    scanf("%d", &divlen);
+
+    len = datalen + divlen - 1;
+
+    printf("\nEnter the data: ");
+    scanf("%s", data);
+
+    printf("\nEnter the divisor: ");
+    scanf("%s", div);
+
+    // Display polynomial form
+    printf("\nPolynomial representation of divisor: ");
+    printPolynomial(div, divlen);
+
+    for (i = 0; i < datalen; i++) {
+        total[i] = data[i];
+        temp[i] = data[i];
+    }
+    for (i = datalen; i < len; i++)
+        total[i] = '0';
+
+    check();
+
+    for (i = 0; i < divlen - 1; i++)
+        temp[i + datalen] = data[i];
+
+    printf("\nTransmitted Code Word: %s", temp);
+    printf("\n\nEnter the received code word: ");
+    scanf("%s", total);
+
+    check();
+    for (i = 0; i < divlen - 1; i++) {
+        if (data[i] == '1') {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        printf("\nSuccessful!!");
+    else
+        printf("\nReceived code word contains errors...\n");
+
+    return 0;
+}
+
+void check() {
+    for (j = 0; j < divlen; j++)
+        data[j] = total[j];
+    while (j <= len) {
+        if (data[0] == '1') {
+            for (i = 1; i < divlen; i++)
+                data[i] = ((data[i] == div[i]) ? '0' : '1');
+        }
+        for (i = 0; i < divlen - 1; i++)
+            data[i] = data[i + 1];
+        data[i] = total[j++];
+    }
+}
+
+void printPolynomial(char divisor[], int length) {
+    int started = 0;
+    for (int i = 0; i < length; i++) {
+        if (divisor[i] == '1') {
+            if (started)
+                printf(" + ");
+            if (i == length - 1)
+                printf("1");
+            else {
+                printf("x^%d", length - i - 1);
+            }
+            started = 1;
+        }
+    }
+    if (!started)
+        printf("0");
+    printf("\n");
+}
